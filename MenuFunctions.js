4129339f4420979e66d7c25797212024ae6b501d
Austin Brooks <austin.williambrooks@gmail.com>
2021-09-02 07:41:02 -0800

    Added start menu and began the works of prompting a user for input

--- MenuFunctions.js
+++ MenuFunctions.js
@@ -1,11 +1,11 @@
 const color = require('./Colors');
-
 const readline = require("readline");
 
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    input: process.stdin,
+    output: process.stdout
+}); 
 
 const funcs = {
     A: function(name){
@@ -17,12 +17,13 @@ const funcs = {
     },
 
     DefaultMenu: function(){
-        console.log(color.green, '  1: Create New Account', '\n', color.cyan, ' 2: Login to your account', '\n', color.red, color.underscore, '3: Close Browser', color.reset);
+        console.log(color.green, '  1: Create New Account', '\n', color.cyan, ' 2: Login to your account', '\n', color.red, color.underscore, '3: Close Browser', color.reset, color.magenta, color.bright);
         //const input = prompt("Enter a valid option 1-3:");
-        console.log(color.magenta);
+        
         rl.question("   What option would you like to select? ", function (answer) {
             if(answer == 1){
                 console.log(color.green, '  Create new account');
+                funcs.getUser();
             }
             else if(answer == 2){
                 console.log(color.cyan, '  Login to your account');
@@ -30,11 +31,24 @@ const funcs = {
             else if(answer == 3){
                 console.log(color.red, '  Closing Browser');
             }
-            rl.close();
+            // rl.close();
           });
+    },
+
+    Login: function(){
+        console.log("Welcome to the login page");
+        //this.getUser();
+    },
+
+
+    getUser: function(){
+        rl.question(color.blue + '   What is your username? ', function(answer) {
+            console.log('   Welcome ' + answer);
+        rl.close();
+        });
     }
 
     
 }
 
-module.exports = funcs;
+module.exports = funcs
